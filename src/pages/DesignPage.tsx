4f9c41657f731e9c5b69c3bec11283de0278171f
Aaronabil <antomi.tomi1979@gmail.com>
2025-05-16 16:43:58 +0700

    nyoba lagi

--- src/pages/DesignPage.tsx
+++ src/pages/DesignPage.tsx
@@ -8,7 +8,7 @@ const designProjects = [
     id: 1,
     title: "Album Cover",
     description: "Photos for music albums.",
-    image: "canva1.png",
+    image: "images/canva1.png",
     link: "#",
     figmaLink: "#",
     type: "Graphic Design"
